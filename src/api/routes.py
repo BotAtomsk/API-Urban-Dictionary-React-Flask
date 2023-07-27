71ce20f5c1cb6532b572b638f3524e8f1e8a7c0f
BotAtomsk <116504982+BotAtomsk@users.noreply.github.com>
2023-07-27 12:31:09 +0000

    Functional basic dictionary

--- src/api/routes.py
+++ src/api/routes.py
@@ -10,18 +10,16 @@ api = Blueprint('api', __name__)
 
 @api.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
-
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     }
-
     return jsonify(response_body), 200
 
 @api.route('/terms', methods=['GET'])
 def get_all_terms():
     terms = Terms.query.all() #en términos obtenemos todos los términos de la bd
     data = [term.serialize() for term in terms] #para cada término lo serializamos (razón: no podemos hacer serialize a una lista de objetos, sino a un objeto)
-    return jsonify(data)
+    return jsonify(data), 200
 
 @api.route('/terms', methods=['POST'])
 def add_term():
@@ -29,4 +27,17 @@ def add_term():
     new_term = Terms(term = body["term"], definition= body["definition"]) #creamos nuevo término a partir de lo que envía el user
     db.session.add(new_term) #decir que se va a añadir a la tabla
     db.session.commit() #añadir la info a la tabla
-    return jsonify({"msg": "new term added succesfully"})
+    return jsonify({"msg": "new term added succesfully"}), 200
+
+@api.route('terms/<term>', methods=['GET'])
+def get_one_term(term):
+    data = Terms.query.filter_by(term = term).first()
+    return jsonify(data.serialize()), 200 #aquí sí podemos decirle que nos devuelva el serialize directamente porque nos está devolviendo un sólo término
+
+@api.route('terms/<string:name>', methods=['DELETE'])
+def delete_individual_term(name): 
+    term = Terms.query.filter_by(term = name).first()
+    db.session.delete(term)
+    db.session.commit()
+    return jsonify({"msg": "término eliminado con éxito"}), 200
+    
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,14 +1,110 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
-	
+	const [allTerms, setAllTerms] = useState([]);
+	const [searchAdd, setSearchAdd] = useState("")
+	const [definition, setDefinition] = useState("")
+	//const [singleTerm, setSingleTerm] = useState({})
+	const [showClearButton, setShowClearButton] = useState(false)
+	const [showIndividualDeleteButton, setShowIndividualDeleteButton] = useState(false)
+	const handleGetAll = async () => {
+		const resp = await fetch("https://botatomsk-studious-space-giggle-wpwpq7p94j6f675-3001.preview.app.github.dev/api/terms")
+		const data = await resp.json()
+		setAllTerms(data)
+		setShowClearButton(true)
+	}
+	const handleAdd = async (e) => {
+		e.preventDefault()
+		const opt = {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				term: searchAdd,
+				definition: definition
+			})
+		}
+		const resp = await fetch("https://botatomsk-studious-space-giggle-wpwpq7p94j6f675-3001.preview.app.github.dev/api/terms", opt)
+		const data = await resp.json()
+		console.log(data)
+		setShowClearButton(false)
+		setDefinition("")
+		setSearchAdd("")
+	}
+	const handleSearch = async (e) => {
+		e.preventDefault()
+		try {
+			const resp = await fetch("https://botatomsk-studious-space-giggle-wpwpq7p94j6f675-3001.preview.app.github.dev/api/terms/" + searchAdd) //para hacer el GET añadimos la variable searchAdd	
+			const data = await resp.json()
+			//setSingleTerm(data)
+			setDefinition(data.definition)
+			setShowIndividualDeleteButton(true)	
+		} catch (error) {
+			setDefinition("The term you are searching for doesn't exist...")
+		}
+		setShowClearButton(true)
+	}
+	const handleClear = async (e) => {
+		e.preventDefault(e)
+		setSearchAdd("")
+		setDefinition("")
+		setShowClearButton(false)
+		setShowIndividualDeleteButton(false)
+	}
+	const handleIndividualDelete = async (e, name) => {
+		e.preventDefault(e)
+		const opt = {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		}
+		const resp = await fetch("https://botatomsk-studious-space-giggle-wpwpq7p94j6f675-3001.preview.app.github.dev/api/terms/" + name, opt)
+		const data = await resp.json()
+		console.log(data)
+		console.log(name)
+		setSearchAdd("")
+		setDefinition("")
+		setShowClearButton(false)
+		setShowIndividualDeleteButton(false)
+	}
+
 	return (
-		<div className="text-center mt-5">
-		
+		<div className="container w-50 text-center">
+			<h1>Urban Dictionary</h1>
+			<button className="btn btn-success" onClick={e=>handleGetAll()}>GET ALL THE DEFINITIONS</button>
+			<form className="form-control">
+				<label htmlFor="term" className="form-text">Term</label>
+				<div className="input-group">
+					<input className="form-control" type="text" name="term" id="term" placeholder="term-to-search/add" value={searchAdd} onChange={e=>setSearchAdd(e.target.value)}/>
+				</div>
+				<label htmlFor="definition" className="form-text">Definition</label>
+				<textarea id="definition" rows="8" className="form-control" value={definition} name="definition" onChange={e=>setDefinition(e.target.value)}></textarea>
+				<div className="d-flex justify-content-between py-3">
+					<button className="btn btn-primary" onClick={e=> handleAdd(e)}>Add</button>
+					{showClearButton && <button className="btn btn-danger" onClick={e=> handleClear(e)}>Clear</button>}
+					{showIndividualDeleteButton && <button className="btn btn-danger" onClick={e=> handleIndividualDelete(e, searchAdd)}>Delete</button>}
+					<button className="btn btn-success" onClick={e=>handleSearch(e)}>Search</button>
+				</div>
+			</form>
+			<div id="conjunto" className="container">
+				{showClearButton && allTerms && allTerms.map(el => <div className="container">
+					<button className="btn btn-danger" onClick={e=> handleIndividualDelete(e, el.term)}>X</button>
+					<p className="fs-4">{el.term}</p>
+					<p>{el.definition}</p>
+				</div>)} 
+				 { /* singleTerm && (         // no quiero que la definición aparezca debajo de la ventana, sino que la voy a llevar a la textform
+					<div className="container">
+						<p className="fs-4">{singleTerm.term}</p>
+						<p>{singleTerm.definition}</p>
+					</div>
+				 ) */} 
+			</div>
 		</div>
 	);
 };
